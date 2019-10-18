6f412c735a46e1c1e3c98d633de872c8c6fa5f49
caomingkai <caomingkai@outlook.com>
2019-10-17 21:40:55 -0700

    add Ember service - 2

--- tests/integration/components/list-filter-test.js
+++ tests/integration/components/list-filter-test.js
@@ -1,35 +1,26 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, settled } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-
-const ITEMS = [{city: 'San Francisco'}, {city: 'Portland'}, {city: 'Seattle'}];
-const FILTERED_ITEMS = [{city: 'San Francisco'}];
 
 module('Integration | Component | list-filter', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('should initially load all listings', async function (assert) {
-    // we want our actions to return promises, since they are potentially
-    // fetching data asynchronously.
-    this.set('filterByCity', () => Promise.resolve({ results: ITEMS }));
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
 
-        // with an integration test, you can set up and use your component in the
-    // same way your application will use it.
+    await render(hbs`<ListFilter />`);
+
+    assert.equal(this.element.textContent.trim(), '');
+
+    // Template block usage:
     await render(hbs`
-      <ListFilter @filter={{action filterByCity}} as |results|>
-        <ul>
-        {{#each results as |item|}}
-          <li class="city">
-            {{item.city}}
-          </li>
-        {{/each}}
-        </ul>
+      <ListFilter>
+        template block text
       </ListFilter>
     `);
-    await settled();
-    assert.equal(this.element.querySelectorAll('.city').length, 3);
-    assert.dom(this.element.querySelector('.city')).hasText('San Francisco');
-  });
 
+    assert.equal(this.element.textContent.trim(), 'template block text');
+  });
 });
